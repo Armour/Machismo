ad2db617f3fbe0c013738be6eb6d227090c581ad
Armour <armour@ArmourdeMacBook-Pro.local>
2015-02-12 23:07:48 +0800

    Initial Commit

---
+++ Machismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Machismo
+//
+//  Created by Armour on 15/2/12.
+//  Copyright (c) 2015年 ZJU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Machismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Machismo
+//
+//  Created by Armour on 15/2/12.
+//  Copyright (c) 2015年 ZJU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
